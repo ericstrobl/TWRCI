5237db5237041e508eff8d8ff1d9a8a3f1aff34b
Eric V. Strobl <ericvonstrobl@gmail.com>
2024-08-12 18:16:21 -0400

    Create MACR_silver_standard.R

---
+++ R/MACR_silver_standard.R
@@ -0,0 +1,11 @@
+MACR_silver_standard <- function(Y,G,exp_d,SNP_d,SNPs,nu_d,desc,exp_d_all){
+  
+  anc = isAncAll(G,1:(ncol(G)-1),ncol(G))
+  
+  desc=setdiff(desc,anc)
+  
+  resids1 = earth( cbind(exp_d[,anc],SNP_d[,SNPs[[ncol(G)]]],nu_d),Y)$residuals
+  resids2 = earth( cbind(exp_d[,anc],SNP_d[,SNPs[[ncol(G)]]],nu_d),exp_d_all[,desc])$residuals
+
+  return( mean(abs(cor(resids1,resids2))) )
+}
